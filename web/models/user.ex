30ec785d64ab25f056d5600892d9eb77e9844b5b
Alvaro <apelegrinafernandez@narwhale.es>
2017-08-15 01:20:32 +0200

    Created user model. Migration added.

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,4 +1,17 @@
 defmodule Rumbl.User do
-  # elixir struct
-  defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+
+  # DSL built with schema and field Elixir macros
+  # :id automatic primary key like Django
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    # virtual field for password
+    # it's an intermediate field for password hash
+    # virtual fields are not persisted to the database
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
 end
